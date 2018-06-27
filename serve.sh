5754b13343ef81cbd0d037b5bb6092c123e87ccd
Juan Pablo <juanpgenovese@gmail.com>
2018-06-27 22:46:18 +0200

    Add dynamic version info

--- serve.sh
+++ serve.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env bash
 
-jekyll serve -w -H 127.0.0.1 -P 4000 -o -l
+export CI_COMMIT_SHA=my-sha
+export GA_ID=my-ga-id
+cat << EOF > _config.ci.yml
+version: "$CI_COMMIT_SHA"
+analytics: "$GA_ID"
+EOF
+jekyll serve -w -H 127.0.0.1 -P 4000 -o -l --config _config.yml,_config.ci.yml
